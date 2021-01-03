36bbd3df7f5116d3fbd39f265a135ee867bf5ba7
SamDC97 <sam.decock97@gmail.com>
2021-01-03 11:44:22 +0100

    Initial commit

---
+++ src/main/java/be/cvo/broodservice/BroodserviceApplication.java
@@ -0,0 +1,13 @@
+package be.cvo.broodservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BroodserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BroodserviceApplication.class, args);
+	}
+
+}
