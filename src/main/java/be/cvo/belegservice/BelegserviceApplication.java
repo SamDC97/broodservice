8ef2c0f9580c279cee862a49654a6d8f6c4a06f0
SamDC97 <sam.decock97@gmail.com>
2021-01-03 11:46:24 +0100

    Initial commit

---
+++ src/main/java/be/cvo/belegservice/BelegserviceApplication.java
@@ -0,0 +1,13 @@
+package be.cvo.belegservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelegserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelegserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/cvo/belegservice/BelegserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package be.cvo.belegservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelegserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
